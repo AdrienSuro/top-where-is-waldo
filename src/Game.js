701859c7e54418d635c2c38ec4329005452d5d7b
Adrien Surowiec <adrien.surowiec@protonmail.com>
2023-05-01 16:35:45 +0200

    add basic css

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,13 @@
 import React from "react";
+import festival from "./img/festival.png";
+import "./style.css";
 
 const Game = () => {
-  return <h1>Game Page</h1>;
+  return (
+    <div>
+      <img className="festivalImg" src={festival}></img>
+    </div>
+  );
 };
 
 export default Game;
--- src/Nav.js
+++ src/Nav.js
@@ -1,10 +1,26 @@
 import React from "react";
+import "./normalize.css";
+import "./style.css";
+import first from "./img/1_104_104.png";
+import second from "./img/2_104_104.png";
+import third from "./img/3_104_104.png";
 
 const Nav = () => {
   return (
-    <ul>
-      <li>Test</li>
-    </ul>
+    <div className="header">
+      <h2 className="newGame">NEW GAME</h2>
+      <h2 className="bestScores">BEST SCORES</h2>
+      <h1>FESTIWALDO</h1>
+      <div className="charactersDiv">
+        <h2>FIND THESE 3 CHARACTERS</h2>
+        <img src={first}></img>
+        <img src={second}></img>
+        <img src={third}></img>
+        <h3></h3>
+        <h3></h3>
+        <h3></h3>
+      </div>
+    </div>
   );
 };
 
