0b815ca4f2b1e02d219ee8cec9089a3d19671278
Adrien Surowiec <adrien.surowiec@protonmail.com>
2023-05-07 19:09:29 +0200

    final win msg works but needs refactoring

--- src/Game.js
+++ src/Game.js
@@ -11,15 +11,13 @@ const Game = (props) => {
   let targetingBoxClicked = false;
   const [clickX, setClickX] = useState(0);
   const [clickY, setClickY] = useState(0);
-  // let clickX = 0;
-  // let clickY = 0;
   const [rect, setRect] = useState("");
-  // let rect = "";
+  const [foundChar, setFoundChar] = useState([]);
+  const [winner, setWinner] = useState(false);
 
   useEffect(() => {
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       setTarget(querySnapshot.docs.map((doc) => doc.data()));
-      console.log("useEffect");
     });
   }, []);
 
@@ -30,9 +28,26 @@ const Game = (props) => {
     };
   }, []);
 
+  useEffect(() => {
+    if (foundChar.length === 3) {
+      displayMsg("complete");
+      console.log("it has length 3 youhou");
+      setWinner(true);
+    } else {
+      displayMsg();
+    }
+  }, [foundChar]);
+
+  function displayMsg(completeness) {
+    const messageBox = document.getElementById("messageBox");
+
+    if (completeness) {
+      messageBox.innerHTML = "YOU WON";
+      console.log("you won in displaymsg");
+    }
+  }
+
   function checkCoord(coord, character) {
-    console.log(coord);
-    console.log(clickX);
     let result = "";
     let characterIndex = target.findIndex((element) => {
       return element.name === character;
@@ -44,6 +59,8 @@ const Game = (props) => {
       coord[1] <= target[characterIndex].ymax
     ) {
       result = target[characterIndex].name;
+      setFoundChar([...foundChar, result]);
+      console.log(foundChar);
     }
     return result;
   }
@@ -54,7 +71,11 @@ const Game = (props) => {
       const characterTarget = document.getElementById(character + "Checkbox");
       characterTarget.innerHTML = "&#10003;";
       characterTarget.style.fontSize = "2.2rem";
-      messageBox.innerHTML = `Congratulations ! You found ${character} `;
+      if (winner === true) {
+        messageBox.innerHTML = `YOU FOUND THEM 3 `;
+      } else {
+        messageBox.innerHTML = `Congratulations ! You found ${character} `;
+      }
     } else {
       console.log("Missed ! Try again");
       messageBox.innerHTML = "You missed the target, try again!";
@@ -62,14 +83,9 @@ const Game = (props) => {
   }
 
   function getPositionRelToImg() {
-    console.log(
-      "Inside getPositionRelToImg, x is " + clickX + " and y is " + clickY
-    );
-    console.log("Inisde GetpositionrELtoImg, rec is " + rect);
     let absoluteX = clickX * (2248 / rect.width);
     let absoluteY = (clickY - 140) * (2248 / rect.width);
     let clickAbsoluteCoord = [absoluteX, absoluteY];
-    console.log(clickAbsoluteCoord);
     return clickAbsoluteCoord;
   }
 
@@ -77,29 +93,29 @@ const Game = (props) => {
     const targetingBox = document.getElementById("targetingbox");
     const messageBox = document.getElementById("messageBox");
     let clickAbsoluteCoord = getPositionRelToImg();
-    console.log(clickAbsoluteCoord);
-    console.log(clickX);
     let result = checkCoord(clickAbsoluteCoord, character);
     checkMsg(result);
     targetingBox.style.visibility = "hidden";
     targetingBoxClicked = false;
     messageBox.style.visibility = "visible";
-    setTimeout(() => {
-      messageBox.style.visibility = "hidden";
-    }, 2000);
+    if (foundChar.length == 3) {
+      console.log("you found them all");
+    } else {
+      setTimeout(() => {
+        messageBox.style.visibility = "hidden";
+      }, 2000);
+    }
   }
 
   function updateClickCoord(x, y) {
     setClickX(x);
     setClickY(y);
-    console.log("x is " + clickX + " and y is " + clickY);
   }
 
   function showTargetingBox(x, y, e) {
     const targetingBox = document.getElementById("targetingbox");
     if (targetingBoxClicked === false) {
       setRect(e.target.getBoundingClientRect());
-      console.log(rect);
       targetingBox.style.top = y - 140 + "px";
       targetingBox.style.left = x + "px";
       targetingBox.style.visibility = "visible";
