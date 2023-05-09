5d1bbc423a27af87c93e79dbb4c629ee6715d382
Adrien Surowiec <adrien.surowiec@protonmail.com>
2023-05-09 13:40:02 +0200

    switch back timer to seconds

--- src/Game.js
+++ src/Game.js
@@ -30,7 +30,7 @@ const Game = (props) => {
   }, []);
 
   useEffect(() => {
-    const intervalFn = setInterval(props.incrementSeconds, 10);
+    const intervalFn = setInterval(props.incrementSeconds, 1000);
     setIntervalId(intervalFn);
     return () => {
       clearInterval(intervalFn);
--- src/Timer.js
+++ src/Timer.js
@@ -2,22 +2,15 @@ import React from "react";
 import { useState, useEffect } from "react";
 
 function Timer(props) {
-  const [ms, setMs] = useState(0);
   const [seconds, setSeconds] = useState(0);
   const [minutes, setMinutes] = useState(0);
 
   useEffect(() => {
-    if (props.elapsedSeconds >= 3600) {
-      setMs(props.elapsedSeconds % 60);
-      setSeconds(parseInt((props.elapsedSeconds % 3600) / 60, 10));
-      setMinutes(parseInt(props.elapsedSeconds / 60 / 60, 10));
-    } else if (props.elapsedSeconds >= 60) {
-      setMs(props.elapsedSeconds % 60);
-      setSeconds(parseInt(props.elapsedSeconds / 60, 10));
-      setMinutes(parseInt(props.elapsedSeconds / 60 / 60, 10));
+    if (props.elapsedSeconds >= 60) {
+      setSeconds(props.elapsedSeconds % 60);
+      setMinutes(parseInt(props.elapsedSeconds / 60, 10));
     } else if (props.elapsedSeconds < 60) {
-      setMs(props.elapsedSeconds);
-      setSeconds(0);
+      setSeconds(props.elapsedSeconds);
       setMinutes(0);
     }
   }, [props.elapsedSeconds]);
@@ -25,7 +18,7 @@ function Timer(props) {
   return (
     <div className="timer">
       {minutes < 10 ? "0" + minutes : minutes} :{" "}
-      {seconds < 10 ? "0" + seconds : seconds} : {ms}
+      {seconds < 10 ? "0" + seconds : seconds}
     </div>
   );
 }
